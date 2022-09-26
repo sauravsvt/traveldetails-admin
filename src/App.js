eaa8c8c8b3377fdfc8f4e5ba41463fca4b529eba
sauravsvt <sauravshriwastavaa@gmail.com>
2022-09-26 20:05:50 +0545

    resolved unnessary imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route , Link, Navigate} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route , Link} from 'react-router-dom'
 import Add from './Pages/Add'
 import Show from './Pages/Show'
 
