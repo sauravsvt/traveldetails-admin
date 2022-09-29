0f08d9eb627a64fe78df94a65be45c408f0c76b3
sauravsvt <sauravshriwastavaa@gmail.com>
2022-09-29 19:36:54 +0545

    added abhisek's css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route , Link} from 'react-router-dom'
 import Add from './Pages/Add'
 import Show from './Pages/Show'
+import './App.css'
 
 function App() {
   return (
 
    <>
     <Router>
-        <nav>
-        <Link to ="/Add">Add Work</Link>
-        <Link to ="/View">Show</Link>
+        <nav className='links'>
+        <Link className='link' to ="/Add">Add Work</Link>
+        <Link className='link' to ="/View">Show</Link>
         </nav>
       
 
--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -58,19 +58,31 @@ function Add() {
   return (
     <>
     <div className="form">
-    <input type="text" 
-        placeholder="name" name="name" value={name} onChange={e => inputHandler(e)} />
-    <input type="text" 
-        placeholder="Task" name="task" value={task} onChange={e => inputHandler(e)} />
-              <br></br>
-    <input type="text" 
-        placeholder="Deadline" name="deadline" value={deadline} onChange={e => inputHandler(e)} />
-          <br></br>
+      <div className="name form-child">
+        <h4>Name:</h4>
+        <input className="dead" type="text" 
+            placeholder="Name" name="name" value={name} onChange={e => inputHandler(e)} />
+      </div>  
+
+      <div className='tasks form-child'>
+        <h4>Task:</h4>
+        <input type="text" 
+          placeholder="Task" name="task" value={task} onChange={e => inputHandler(e)} />
+                
+      </div>
       
+      <div className="dead-line form-child">
+        <h4>DeadLine:</h4>   
+        <input type="text" 
+            placeholder="Deadline" name="deadline" value={deadline} onChange={e => inputHandler(e)} />
+              
+      </div>  
+
+      <div className="buttons">
+        <button onClick={onSubmit}>Submit</button>
+        <button onClick={viewData}>Show All</button>
+      </div>  
     </div>
-    <button onClick={onSubmit}>Submit</button>
-    <button onClick={viewData}>Show All</button>
-    
     <ToastContainer />
 
   </>
